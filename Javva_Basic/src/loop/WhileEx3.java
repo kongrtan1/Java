ab17a9df805ade1f40b0199022fde6fc4f83860c
unknown <sjjw0629@gmail.com>
2024-05-30 01:34:50 +0900

    java study

---
+++ Javva_Basic/src/loop/WhileEx3.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class WhileEx3 {
+    public static void main(String[] args) {
+        int num = 0;
+        int count = 0;
+
+        while(count <10){
+            System.out.println(num+=2);
+            count++;
+        }
+    }
+}
---
+++ ch02/src/Ex5_1_tmp.java
@@ -0,0 +1,26 @@
+public class Ex5_1_tmp {
+  public static void main(String[] args) {
+//    int[] score; // 1. 배열 score를 선언(참조변수)
+//    score = new int[5]; // 2. 배열의 생성 (int저장공간 x5)
+
+//    int[] score = new int[5]; //배열의 선언과 생성을 동시에
+//    score[3] = 100;
+//
+//    System.out.println("score[0]=" +score[0]);
+//    System.out.println("score[1]=" +score[1]);
+//    System.out.println("score[2]=" +score[2]);
+//    System.out.println("score[3]=" +score[3]);
+//    System.out.println("score[4]=" +score[4]);
+//
+//    int value = score[3];
+//    System.out.println("value="+value);
+
+//    int[] arr = new int[10]; //길이가 5인 int 배열 arr생성
+//    System.out.println("arr.length="+ arr.length);
+//
+//    for(int i=0; i<arr.length; i++) {
+//      System.out.println("arr["+i+"]="+arr[i]);
+//    }
+
+  }
+}
