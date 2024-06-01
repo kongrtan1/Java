21ccce7245b6beb0acb17d9ef88f9a1b71a09bb9
kongrtan1 <kongrtan92@naver.com>
2024-06-01 21:20:11 +0900

    java study

--- Javva_Basic/src/loop/For3.java
+++
@@ -1,9 +0,0 @@
-package loop;
-
-public class For3 {
-    public static void main(String[] args) {
-        for(int num = 2, count = 1; count<=10; num+=2, count++){
-            System.out.println(num);
-        }
-    }
-}
