0abac4e350501089f9065c07a3aa4839982f3ffa
kongrtan1 <kongrtan92@naver.com>
2024-06-01 21:12:56 +0900

    java study

--- ch02/src/Ex4_12.java
+++
@@ -1,9 +0,0 @@
-public class Ex4_12 {
-  public static void main(String[] args) {
-    int i = 5; // 반복 할 횟수.
-
-    while(i--!=0) {
-      System.out.println(i + " - I can do it.");
-    }
-  }
-}
--- ch02/src/Ex4_8.java
+++
@@ -1,9 +0,0 @@
-public class Ex4_8 {
-  public static void main(String[] args) { // i=1,2,3
-    int i=1; // scope (범위)
-
-    for (; i <= 10; i++) {
-      System.out.println(i);
-    }
-  }
-}
