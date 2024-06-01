8dca72ed7749d8fe4fcabf634262948f58b99be9
kongrtan1 <kongrtan92@naver.com>
2024-06-01 21:07:10 +0900

    java study

--- Javva_Basic/src/array/ex/ArrayEx9.java
+++ Javva_Basic/src/array/ex/ArrayEx9.java
@@ -11,11 +11,17 @@ public class ArrayEx9 {
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("1. 상품 등록 ㅣ 2. 상품 목록 ㅣ 2. 종료 \n메뉴를 선택하세요:");
+            System.out.print("1. 상품 등록 ㅣ 2. 상품 목록 ㅣ 3. 종료 \n메뉴를 선택하세요:");
             int menu = scanner.nextInt();
             scanner.nextLine();
 
             if (menu == 1) {
+
+                if (productCount >= maxProducts) {
+                    System.out.println("더 이상 상품을 등록할 수 없습니다.");
+                    continue;
+                }
+
                 System.out.print("상품 이름을 입력하세요:");
                 productNames[productCount] = scanner.nextLine();
 
@@ -23,6 +29,19 @@ public class ArrayEx9 {
                 productPrices[productCount] = scanner.nextInt();
 
                 productCount++;
+            } else if (menu == 2) {
+                if(productCount == 0) {
+                    System.out.println("등록된 상품이 없습니다.");
+                    continue;
+                }
+                for (int i = 0; i < productCount; i++) {
+                    System.out.println(productNames[i] + ": " + productPrices[i] + "원");
+                }
+            }else if (menu == 3) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else {
+                System.out.println(("잘못된 메뉴를 선택하셨습니다."));
             }
         }
     }
