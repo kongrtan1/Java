d86d52e7aedd341dec8876be24f4cebc9e9f3a42
kongrtan1 <kongrtan92@naver.com>
2024-06-18 00:10:19 +0900

    java study

--- java-basic/src/class1/ClassStart5.java
+++ java-basic/src/class1/ClassStart5.java
@@ -13,11 +13,18 @@ public class ClassStart5 {
         student2.age = 16;
         student2.grade = 80;
 
-        Student[] students = {student1, student2};
+        Student[] students = new Student[]{student1, student2};
 
         for (int i = 0; i < students.length; i++) {
             System.out.println("이름:" + students[i].name + " 나이:" + students[i].age + " 성적:" + students[i].grade);
-
+        }
+        for (int i = 0; i < students.length; i++) {
+            Student s = students[i];
+            System.out.println("이름:" + s.name + " 나이:" + s.age + " 성적:" + s.grade);
+        }
+        for (Student s : students) {
+            System.out.println("이름:" + s.name + " 나이:" + s.age + " 성적:" + s.grade);
         }
     }
 }
+
